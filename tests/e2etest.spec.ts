f82fbe08fa2e6aa7d2e811bfa4b1ef6e69fd4be0
George Townley <george@knowlesavenue.com>
2025-06-21 15:51:04 +0100

    add playwright e2e test

--- tests/e2etest.spec.ts
+++ tests/e2etest.spec.ts
@@ -7,9 +7,10 @@ test("test", async ({ page }) => {
     .click();
   await page
     .getByRole("textbox", { name: "https://example.com/very/long" })
-    .fill("https://www.google.com/");
-  await page.getByRole("button", { name: "submit" }).click();
+    .fill("https://google.com");
+  await page.getByRole("button", { name: "Create Short URL" }).click();
   const page1Promise = page.waitForEvent("popup");
   await page.getByRole("link", { name: "Test your short URL →" }).click();
   const page1 = await page1Promise;
+  await page1.getByRole("button", { name: "Accept all" }).click();
 });
